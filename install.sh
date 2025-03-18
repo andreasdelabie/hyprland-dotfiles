a259ea063e3c80971ebc86660b5d989686aa3ed3
andreasdelabie <andreas.delabie@gmail.com>
2025-03-18 18:17:29 +0000

    Updated install script

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ ANDREAS' WAYLAND DOTFILES
 -------------------------
 """
 
+sleep 2s
+
 echo "[INSTALLING PACKAGES...]"
 sudo pacman -S ly hyprland waybar wofi pulseaudio gnome-terminal otf-font-awesome
 
@@ -14,4 +16,7 @@ cp -r ./.config/wofi /home/$USER/.config/
 cp -r ./.config/waybar /home/$USER/.config/
 
 echo "[FINISHING INSTALL...]"
+sudo systemctl enable ly.service
 pulseaudio -D
+
+echo "[FINISHED INSTALLING!]
--- update.sh
+++ update.sh
@@ -2,3 +2,7 @@ mkdir -p ./.config/
 cp -r /home/andreas/.config/hypr/ ./.config/
 cp -r /home/andreas/.config/wofi/ ./.config/
 cp -r /home/andreas/.config/waybar/ ./.config/
+
+read -p "Enter commit message: " message
+git add -A
+git commit -m "$message"
