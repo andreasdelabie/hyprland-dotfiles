f5cbbf7f8c92d7be26f9fc10a6d9ff82be4d8429
andreasdelabie <andreas.delabie@gmail.com>
2025-03-19 08:38:11 +0000

    Added scripts folder

--- install.sh
+++ install.sh
@@ -7,21 +7,20 @@ ANDREAS' WAYLAND DOTFILES
 sleep 2s
 
 echo "[INSTALLING PACKAGES...]"
-sudo pacman -S ly hyprland waybar wofi pulseaudio gnome-terminal otf-font-awesome
+sudo pacman -S ly hyprland waybar wofi gnome-terminal otf-font-awesome
 
 echo "[COPYING DOT FILES...]"
-mkdir -p /home/$USER/.config/
-cp -r ./.config/hypr /home/$USER/.config/
-cp -r ./.config/wofi /home/$USER/.config/
-cp -r ./.config/waybar /home/$USER/.config/
+mkdir -p $HOME/.config/
+cp -r ./.config/hypr $HOME/.config/
+cp -r ./.config/wofi $HOME/.config/
+cp -r ./.config/waybar $HOME/.config/
 
 echo "[COPYING BACKGROUNDS...]"
-sudo mkdir -p /usr/share/backgrounds/
+sudo mkdir -p /usr/share/
 sudo cp -r ./usr/share/backgrounds /usr/share/
 
 echo "[FINISHING INSTALL...]"
 sudo systemctl enable ly.service
-pulseaudio -D
-/usr/share/backgrounds/load-background.sh
+$HOME/.config/hypr/scripts/load-background.sh
 
 echo "[FINISHED INSTALLING!]"
--- update.sh
+++ update.sh
@@ -1,11 +1,18 @@
+# ------------------------------
+#              COPY
+# ------------------------------
 mkdir -p ./.config/
 cp -ru /home/andreas/.config/hypr ./.config/
 cp -ru /home/andreas/.config/wofi ./.config/
 cp -ru /home/andreas/.config/waybar ./.config/
 
-mkdir -p ./usr/share/backgrounds
+mkdir -p ./usr/share/
 cp -ru /usr/share/backgrounds ./usr/share/
 
+
+# ------------------------------
+#         PUSH TO GITHUB
+# ------------------------------
 git add -A
 read -p "Enter commit message: " message
 git commit -m "$message"
